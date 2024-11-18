45dd1fbd08c118acd45a9ad1fce4344984bbe170
Siddharth Sahu <46642995+siddharth8763@users.noreply.github.com>
2024-11-18 10:01:59 +0530

    added self filter method

--- 1-Pollyfils/3-Filter.js
+++ 1-Pollyfils/3-Filter.js
@@ -14,3 +14,11 @@ let arr = [11, 12, 13, 4, 5];
 let op = arr.myFilter((data, index) => data > 4 && index > 1);
 console.log(op);
 
+const array = [{id: 1, name:'vijay'}, {id: 2, name:'Ayush'}, {id: 3, name:'vijay'}];
+
+// Using a map to keep track of unique names
+const uniqueArray = array.filter((obj, index, self) =>
+  index === self.findIndex((el) => el.name === obj.name)
+);
+
+console.log(uniqueArray);
