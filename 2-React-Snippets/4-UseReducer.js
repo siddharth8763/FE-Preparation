61547517beeeadacbb80976d7db5bcd90c5e96df
Siddharth Sahu <46642995+siddharth8763@users.noreply.github.com>
2025-04-27 11:49:39 +0530

    Update 4-UseReducer.js
    Updated with a little advanced example

--- 2-React-Snippets/4-UseReducer.js
+++ 2-React-Snippets/4-UseReducer.js
@@ -1,28 +1,40 @@
 // useReducer is very similar to useState, but it lets you move the state update logic 
 // from event handlers into a single function outside of your component. 
 
+import React, { useReducer } from 'react';
+import './style.css';
+
 function reducer(state, action) {
-  if (action.type === "incremented_age") {
+  if (action.type === `inc_data`) {
     return {
-      age: state.age + 1,
+      ...state,
+      data: state.data + 1,
+    };
+  } else if (action.type === `ìnc_ct_times`) {
+    return {
+      ...state,
+      clickedTimes: state.clickedTimes + 1,
     };
   }
-  throw Error("Unknown action.");
+  return state;
 }
 
-export default function Counter() {
-  const [state, dispatch] = React.useReducer(reducer, { age: 42 });
+export default function App() {
+  const [state, dispacth] = useReducer(reducer, {
+    data: 0,
+    clickedTimes: 0,
+  });
 
   return (
-    <>
-      <button
-        onClick={() => {
-          dispatch({ type: "incremented_age" });
-        }}
-      >
-        Increment age
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <h1>data: {state.data}</h1>
+      <h1>clikedTimes: {state.clickedTimes}</h1>
+      <button onClick={() => dispacth({ type: `inc_data` })}>+data</button>
+      <button onClick={() => dispacth({ type: `ìnc_ct_times` })}>
+        +clikedTimes
       </button>
-      <p>Hello! You are {state.age}.</p>
-    </>
+    </div>
   );
 }
